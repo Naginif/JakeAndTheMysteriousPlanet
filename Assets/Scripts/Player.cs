9e21317b4fda107bae448aceefae4529441a3832
Dan Finigan <danielfinigan@gmail.com>
2016-10-03 22:52:34 -0400

    Created loop to generate ground across the entire level, created player with jump ability while grounded

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,58 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-    private Rigidbody rb;
-	// Use this for initialization
-	void Start ()
+    private Rigidbody2D rb;
+
+    public float jumpForce = 6f;
+    public float runningSpeed = 1.5f;
+    public LayerMask groundLayer;
+    public Animator animator;
+    // Use this for initialization
+
+    void Awake()
     {
-        Instantiate(Resources.Load("FloorPiece"), 
-                    new Vector3(-7, -4, 0),
-                    Quaternion.Euler(90, 0, 0));
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start ()
+    {
+
+        //animator.SetBool("isAlive", true);
+
+        for (int x = -9; x <= 9; x += 1)
+        {
+            Instantiate(Resources.Load("GroundMid"),
+                        new Vector2(x, -4), 
+                        Quaternion.identity);
+
+        }
 	}
 	
+    void FixedUpdate()
+    {
+        if (rb.velocity.x < runningSpeed)
+            rb.velocity = new Vector2(runningSpeed, rb.velocity.y);
+    }
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))        
+            Jump();
+
+        //animator.SetBool("isGrounded", IsGrounded());
 	}
+
+    void Jump()
+    {
+        if(IsGrounded())
+             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    
+    bool IsGrounded()
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 1, groundLayer.value))
+            return true;
+        else
+            return false;
+    }
 }
