64eb6a3a4f064e88916fa47c0caa472ea4c12b5a
Dan Finigan <danielfinigan@gmail.com>
2016-10-03 21:11:18 -0400

    Created Prefabs, Materials, Scenes, Scripts, and Sprites Folders as well as Player and FloorPiece Prefabs

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start ()
+    {
+        Instantiate(Resources.Load("FloorPiece"), 
+                    new Vector3(-7, -4, 0),
+                    Quaternion.Euler(90, 0, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
