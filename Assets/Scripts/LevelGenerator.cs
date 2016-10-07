8f39769ee881ddf797b2a5c8101e70ffe4780abd
Dan Finigan <danielfinigan@gmail.com>
2016-10-06 21:27:00 -0400

    Corrected position of LevelPieceBasic to equal it's startpoint value, levelPieces now successfully connect!

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,24 +5,30 @@ using System.Collections.Generic;
 public class LevelGenerator : MonoBehaviour {
     public static LevelGenerator instance;
     public List<LevelPiece> levelPrefabs = new List<LevelPiece>();
+    public LevelPiece Element0;
     public List<LevelPiece> pieces = new List<LevelPiece>();
     public Transform levelStartPoint;
 
     public void AddPiece()
     {
         int randomIndex = Random.Range(0, levelPrefabs.Count);
-
         LevelPiece piece = (LevelPiece)Instantiate(levelPrefabs[randomIndex]);
         piece.transform.SetParent(this.transform, false);
 
         Vector3 spawnPosition = Vector3.zero;
 
         if (pieces.Count == 0)
+        {
             spawnPosition = levelStartPoint.position;
+            Debug.Log("Count = 0!");
+        }
         else
+        {
             spawnPosition = pieces[pieces.Count - 1].exitPoint.position;
+        }
 
         piece.transform.position = spawnPosition;
+        Debug.Log("spawnposition = " + spawnPosition.ToString());
         pieces.Add(piece);
     }
 
@@ -35,6 +41,14 @@ public class LevelGenerator : MonoBehaviour {
     {
 
     }
+ 
+    public void GenerateInitialPieces()
+    {
+        levelPrefabs.Add(Element0);
+        for(int i = 0; i < 2; i++)
+            AddPiece();
+        Debug.Log("levelPrefab count is: " + levelPrefabs.Count);
+    }
 
     void Awake()
     {
@@ -42,7 +56,7 @@ public class LevelGenerator : MonoBehaviour {
     }
     // Use this for initialization
     void Start () {
-	
+        GenerateInitialPieces();
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/LevelPiece.cs
+++ Assets/Scripts/LevelPiece.cs
@@ -2,20 +2,5 @@
 using System.Collections;
 
 public class LevelPiece : MonoBehaviour {
-
     public Transform exitPoint;
-	// Use this for initialization
-	void Start () {
-        /*  for (int x = -9; x <= 4; x += 1)
-      {
-          Instantiate(Resources.Load("GroundMid"),
-                      new Vector2(x, -2), 
-                      Quaternion.identity);
-      }*/
-    }
-
-    // Update is called once per frame
-    void Update () {
-	
-	}
 }
